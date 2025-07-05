c3499f5fffd6acad1da93f203580c093de8cb592
fridem <nik_dantexxl@mail.ru>
2025-07-06 00:11:59 +0300

    ver 2

--- app_updater/make_zip.py
+++ app_updater/make_zip.py
@@ -1,9 +1,11 @@
 import zipfile
 import os
 
-APP_EXE = "app.exe"
+APP_FOLDER = "app"
 VERSION = "1.0.6"
 ZIP_NAME = "app.zip"
+folder_path = f'D:/Python/git_loc/app_updater/output/{APP_FOLDER}'
+output_path = f'D:/Python/git_loc/app_updater/output/{ZIP_NAME}'
 
 # Создаем версию
 with open("version.txt", "w") as f:
@@ -12,8 +14,11 @@ with open("version.txt", "w") as f:
 if os.path.exists(ZIP_NAME):
     os.remove(ZIP_NAME)
 
-with zipfile.ZipFile(ZIP_NAME, 'w') as zipf:
-    zipf.write(APP_EXE)
-    zipf.write("version.txt")
+with zipfile.ZipFile(output_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            abs_path = os.path.join(root, file)
+            rel_path = os.path.relpath(abs_path, folder_path)
+            zipf.write(abs_path, rel_path)
 
 print(f"[✓] Архив {ZIP_NAME} успешно создан.")
