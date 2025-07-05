624cb2992d00055334b6cad8b4e250ab84d63d8b
fridem <nik_dantexxl@mail.ru>
2025-07-05 23:47:22 +0300

    request

--- pythonProject/test.py
+++ pythonProject/test.py
@@ -1,10 +1,23 @@
+import logging
+import random
+
+logging.basicConfig(format = '%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
+                    level=logging.DEBUG, filename='test.log', filemode='a+', encoding='utf-8')
+
+
 def test():
-    print('TEST hello!')
+    return random.randint(0, 1)
 
 
-def uau():
-    print('Uwuuuu')
+def request():
+    res = test()
+    if res:
+        logging.debug('Успешно')
+        print('Обработка успешного результата')
+    else:
+        logging.debug('Провал')
+        print('Обработка негативного результата')
 
 
-uau()
-test()
+if __name__ == '__main__':
+    request()
