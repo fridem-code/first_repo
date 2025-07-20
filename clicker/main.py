df2b8dcdbf3b9b544a40265f21cbdc64d1b15fa3
fridem <nik_dantexxl@mail.ru>
2025-07-21 01:44:42 +0300

    clicker перенёс в класс App

--- clicker/main.py
+++ clicker/main.py
@@ -5,82 +5,102 @@ import customtkinter as ctk
 import keyboard  # Для отслеживания нажатий клавиш
 import json
 
-# Переменная для контроля состояния кликера
-running = False
 
-def clicker(click_interval):
-    """Функция для выполнения кликов мыши с заданным интервалом."""
-    global running
-    while running:
-        pyautogui.click()  # Выполняем клик
-        time.sleep(click_interval)  # Пауза между кликами
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
 
-# Функция для запуска кликера
-def start_clicker():
-    global running
-    click_interval = float(interval_entry.get())  # Получаем интервал из поля
-    running = True
-    clicking_thread = threading.Thread(target=clicker, args=(click_interval,))
-    clicking_thread.start()
-    # Запускаем поток для проверки нажатия клавиши 'q' только при запуске кликера
-    check_stop_thread()
+        self.title('Мой кликер. BETA')
+        self.geometry('400x400')
+        self.center_window()
 
-def check_stop_key():
-    """Функция для остановки кликера по нажатию клавиши 'q'."""
-    while running:
-        if keyboard.is_pressed('q'):  # Если нажата клавиша 'q'
-            stop_clicker()  # Останавливаем кликер
-            print("Кликер остановлен нажатием 'q'.")
-            break  # Выходим из цикла
+        self.resizable(False, False)
 
-def check_stop_thread():
-    """Запуск потока для проверки клавиши остановки."""
-    stop_thread = threading.Thread(target=check_stop_key)
-    stop_thread.daemon = True  # Делая поток демоном, он закрывается при закрытии основного окна
-    stop_thread.start()
+        self.main_frame = ctk.CTkFrame(self, corner_radius=0, fg_color='transparent')
+        self.main_frame.pack(anchor='center', expand=True)
 
-# Функция для остановки кликера
-def stop_clicker():
-    global running
-    running = False
+        self.start_btn = ctk.CTkButton(self.main_frame, text='Запустить', command=self.start_clicker)
+        self.start_btn.pack()
+
+        self.interval_entry = ctk.CTkEntry(self.main_frame, placeholder_text='0.01')
+        self.interval_entry.pack(pady=5)
+
+        self.stop_button = ctk.CTkButton(self.main_frame, text="Остановить", command=self.stop_clicker)
+        self.stop_button.pack(pady=5)
+
+        self.exit_button = ctk.CTkButton(self.main_frame, text="Выход", command=self.quit)
+        self.exit_button.pack(pady=5)
+
+        ctk.set_appearance_mode("dark")  # Устанавливаем темную тему
+        ctk.set_default_color_theme("blue")  # Устанавливаем цветовую тему
+
+        self.start_app()
+
+    def center_window(self):
+        w = 400
+        h = 400
+
+        sw = self.winfo_screenwidth()
+        sh = self.winfo_screenheight()
+
+        x = (sw - w) / 2
+        y = (sh - h) / 2
+        self.geometry('%dx%d+%d+%d' % (w, h, x, y))
+
+    def clicker(self, click_interval):
+        """Функция для выполнения кликов мыши с заданным интервалом."""
+        while self.running:
+            pyautogui.click()  # Выполняем клик
+            time.sleep(click_interval)  # Пауза между кликами
+
+    def start_clicker(self):
+        try:
+            click_interval = float(self.interval_entry.get())  # Получаем интервал из поля
+        except ValueError:
+            print("Некорректный интервал. Введите число.")
+            return
+
+        self.running = True
+        clicking_thread = threading.Thread(target=self.clicker, args=(click_interval,))
+        clicking_thread.start()
+        # Запускаем поток для проверки нажатия клавиши 'q' только при запуске кликера
+        self.check_stop_thread()
+
+    def check_stop_key(self):
+        """Функция для остановки кликера по нажатию клавиши 'q'."""
+        while self.running:
+            if keyboard.is_pressed('q'):  # Если нажата клавиша 'q'
+                self.stop_clicker()  # Останавливаем кликер
+                print("Кликер остановлен нажатием 'q'.")
+                break  # Выходим из цикла
+
+    def check_stop_thread(self):
+        """Запуск потока для проверки клавиши остановки."""
+        stop_thread = threading.Thread(target=self.check_stop_key)
+        stop_thread.daemon = True  # Делая поток демоном, он закрывается при закрытии основного окна
+        stop_thread.start()
+
+    def stop_clicker(self):
+        global running
+        running = False
+
+    # Функция для остановки кликера
+    def load_config(self):
+        try:
+            with open('config.json', 'r') as f:
+                config = f.read()
+                config_data = json.loads(config)
+                interval_click_time = config_data['interval_click_time']
+        except json.JSONDecodeError as exc:
+            print(f'Возникла ошибка чтение config.json:\n{exc}')
+
+    def start_app(self):
+        # Переменная для контроля состояния кликера
+        self.running = False
 
 
-def load_config():
-    try:
-        with open('config.json', 'r')as f:
-            config = f.read()
-            config_data = json.loads(config)
-            interval_click_time = config_data['interval_click_time']
-    except json.JSONDecodeError as exc:
-        print(f'Возникла ошибка чтение config.json:\n{exc}')
-
-# Создаем корневое окно
-ctk.set_appearance_mode("dark")  # Устанавливаем темную тему
-ctk.set_default_color_theme("blue")  # Устанавливаем цветовую тему
-
-root = ctk.CTk()  # Создаем экземпляр окна
-root.title("Супер быстрый кликер")
-
-# Создаем элементы интерфейса
-title_label = ctk.CTkLabel(root, text="Супер быстрый кликер")
-title_label.pack(pady=10)
-
-interval_label = ctk.CTkLabel(root, text="Интервал клика (секунды):")
-interval_label.pack(pady=5)
-
-interval_entry = ctk.CTkEntry(root, placeholder_text="0.01")
-interval_entry.pack(pady=5)
-
-start_button = ctk.CTkButton(root, text="Запустить", command=start_clicker)
-start_button.pack(pady=5)
-
-stop_button = ctk.CTkButton(root, text="Остановить", command=stop_clicker)
-stop_button.pack(pady=5)
-
-exit_button = ctk.CTkButton(root, text="Выход", command=root.quit)
-exit_button.pack(pady=5)
-
-# Запускаем основной цикл обработки событий
-root.mainloop()
+if __name__ == '__main__':
+    app = App()
+    app.mainloop()
 
 print("Программа завершена.")
